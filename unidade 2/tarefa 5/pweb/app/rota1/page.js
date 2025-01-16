70a1cf0e24a254bc6ca42bcfc7a9ab7d9ec828a9
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2025-01-16 16:20:31 -0300

    tarefa 5

---
+++ unidade 2/tarefa 5/pweb/app/rota1/page.js
@@ -0,0 +1,11 @@
+import { Saudacao, Despedida } from "../page";
+
+export default function Rota1() {
+    return (
+        <div>
+            <h1>Bem-vindo à Rota 1!</h1>
+            <Saudacao />
+            <Despedida />
+        </div>
+    );
+}
